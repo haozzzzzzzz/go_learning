9c115ef75ec11feb4039822cc8bc486da118dc48
haozzzzzzzz <990747088@qq.com>
2018-01-23 19:48:13 +0800

    kafka订阅。

--- src/kafka/producer_async.go
+++ src/kafka/producer_async.go
@@ -2,35 +2,29 @@ package main
 
 import (
 	"github.com/Shopify/sarama"
-	"fmt"
 )
 
 func main()  {
 
 	config := sarama.NewConfig()
-	config.Producer.Return.Successes = true
+	//config.Producer.Return.Successes = true
 	config.Producer.RequiredAcks = sarama.WaitForAll
 	config.Producer.Partitioner = sarama.NewRandomPartitioner
 
-	producer, err := sarama.NewSyncProducer([]string{"10.5.1.82:9092"}, config)
+	producer, err := sarama.NewAsyncProducer([]string{"10.5.1.82:9092"}, config)
 	if err != nil {
 		panic(err)
-	    return
+		return
 	}
+
 	defer producer.Close()
 
 	msg := &sarama.ProducerMessage{
 		Topic: "service-user-service",
 	}
 
-	for {
+	for i := uint32(0); i < 10; i ++{
 		msg.Value = sarama.ByteEncoder("nihao")
-		partition, offset, err := producer.SendMessage(msg)
-		if err != nil {
-			fmt.Println("send message faild.")
-		    return
-		}
-
-		fmt.Printf("Partition = %d, offset = %d \n", partition, offset)
+		producer.Input() <- msg
 	}
 }
